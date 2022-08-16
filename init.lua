2662aaa392e6737d11fc4d6bbe3dedb8f81326ea
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-08-16 18:07:35 +0100

    Remove comments

--- init.lua
+++ init.lua
@@ -1,11 +1,5 @@
--- Plugin Manager
-require "user.plugins"
-
--- General Options
 require "user.autocommands"
 require "user.colorscheme"
 require "user.keymaps"
 require "user.options"
-
--- LSP
---require "user.lsp"
+require "user.plugins"
